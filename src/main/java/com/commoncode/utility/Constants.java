56ff858780d3b8a385bfee88739e2cb4825a67b4
Kaustubharas <kaustubh_a@techjoomla.com>
2020-04-05 19:55:39 +0530

    Update Constants.java

--- src/main/java/com/commoncode/utility/Constants.java
+++ src/main/java/com/commoncode/utility/Constants.java
@@ -1,9 +1,12 @@
 package com.commoncode.utility;
 
 public class Constants {
-	
-	private Constants() {}
-	
+
+	private Constants() {
+	}
+
+	public static String TESTDATAEXCELFILE = System.getProperty("testDataExcelFile");
+
 	public static final String SITEURL = System.getProperty("SITEURL");
 
 }
